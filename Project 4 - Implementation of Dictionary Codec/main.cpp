94ea00f6bed75a5d925b4ab44b489e5840340a67
Rojan Karn <rokarn12@gmail.com>
2023-03-15 19:21:43 -0400

    added multi threading code

--- Project 4 - Implementation of Dictionary Codec/main.cpp
+++ Project 4 - Implementation of Dictionary Codec/main.cpp
@@ -2,26 +2,104 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
+#include <thread>
+#include <algorithm>
 #include <fstream>
 #include "PrefixTree.h"
 
 using namespace std;
 
-// Parse input file and return a vector of strings
-vector<string> parseFile(const string& filename) {
+#define NUM_WORKER_THREADS 8
+
+void buildPrefixTree(PrefixTree& tree, const vector<string>& lines);
+
+/////////////////////////
+//   WorkerThread Class
+////////////////////////
+
+class WorkerThread {
+    public:
+        WorkerThread() {
+            this->p_thread_id = -1;
+            this->p_launched = false;
+        }
+
+        WorkerThread(const int16_t thread_id) {
+            this->p_thread_id = thread_id;
+            this->p_launched = false;
+        }
+
+        void launch(const vector<string>& lines) {
+            this->p_thread = thread(buildPrefixTree, workerTree, lines);
+            this->p_launched = true;
+        }
+
+        void join() {
+            this->p_thread.join();
+        }
+
+        bool isCompleted() {
+            return !(this->p_launched) || this->p_thread.joinable();
+        }
+
+        void clear() {
+            this->p_thread_id = -1;
+            this->p_launched = false;
+        }
+
+        int16_t getID() {
+            return this->p_thread_id;
+        }
+
+        PrefixTree getTree() {
+            return this->workerTree;
+        }
+
+    private:
+        int16_t p_thread_id;
+        bool p_launched;
+        thread p_thread;
+        PrefixTree workerTree;
+};
+
+////////////////////////////
+//   END WorkerThread Class
+///////////////////////////
+
+// function for threads to use to build trees
+void buildPrefixTree(PrefixTree& tree, const vector<string>& lines) {
+    for (int i = 0; i < lines.size(); i++) {
+        tree.insert(lines[i], i);
+        if (i % 1000 == 0) cout << "Inserted " << i << " words" << endl;
+    }
+}
+
+// Parse input file in chunks and return a vector of strings
+vector<string> parseFile(const string& filename, const int& chunk_size) {
     vector<string> lines;
     ifstream file(filename);
+
     if (!file.is_open()) {
         cout << "Could not open file: " << filename << endl;
         exit(1);
     }
     string line;
-    while (getline(file, line)) {
-        lines.push_back(line);
+    // while (getline(file, line)) {
+    //     lines.push_back(line);
+    // }
+    // reads file in chunks instead of reading the file out completely
+    for (int i = 0; i < chunk_size; i++) {
+        if (getline(file, line)) {
+            lines.push_back(line);
+        } else { // end of file
+            //lines.push_back("end of file"); // not sure how to indicate end of file when reading in chunks
+            break;
+        }
     }
     file.close();
     return lines;
 }
+
 
 // Parse command line arguments
 // Format: filename -arg1 value1 -arg2 value2 ...
@@ -103,15 +181,10 @@ void mergeNodes(PrefixTreeNode* node1, PrefixTreeNode* node2) {
 }
 
 // function to merge two trees
-PrefixTree mergeTrees(PrefixTree& tree1, PrefixTree& tree2) {
+void mergeTrees(PrefixTree& tree1, PrefixTree& tree2) {
     // merge all nodes from tree2 into tree1
-    mergeNodes(tree1.root, tree2.root);
-
-    // return tree
-    PrefixTree ret_tree = tree1;
-
-    // return merged tree
-    return ret_tree;
+    mergeNodes(tree1.root, tree2.root); // call helper function
+    // tree1 is the merged tree
 }
 
 
@@ -131,50 +204,145 @@ vector<string> vanillaSearch(const vector<string>& lines, string prefix) {
 int main(int argc, char* argv[]) {
     // Parse command line arguments
     unordered_map<string, string> args = parseArgs(argc, argv);
-    
-    // Parse input file
-    cout << "Parsing input file..." << endl;
-    vector<string> lines = parseFile(args["filename"]);
-    cout << "Successfully parsed input file!" << endl;
 
-    // Build prefix tree
-    cout << "Building prefix tree..." << endl;
-    PrefixTree tree;
-    for (int i = 0; i < lines.size(); i++) {
-        tree.insert(lines[i], i);
-        if (i % 1000 == 0) cout << "Inserted " << i << " words" << endl;
+    // count number of lines in the file
+    ifstream file(args["filename"]);
+    int num_lines = count(istreambuf_iterator<char>(file), istreambuf_iterator<char>(), '\n') + 1;
+
+    // determine how much of the file each thread will work with
+    int chunk_size = num_lines / NUM_WORKER_THREADS;
+    
+    // // Parse input file
+    // cout << "Parsing input file..." << endl;
+    // vector<string> lines = parseFile(args["filename"], chunk_size);
+    // cout << "Successfully parsed input file!" << endl;
+
+    // // Build prefix tree
+    // cout << "Building prefix tree..." << endl;
+    // PrefixTree tree;
+    // for (int i = 0; i < lines.size(); i++) {
+    //     tree.insert(lines[i], i);
+    //     if (i % 1000 == 0) cout << "Inserted " << i << " words" << endl;
+    // }
+    // cout << "Successfully built prefix tree!" << endl;
+
+    // // Print prefix tree
+    // tree.print();
+
+    // // Search for word "apple"
+    // vector<int> indices = tree.search("apple");
+    // cout << endl;
+    // printVector(indices);
+
+    // // Search for prefix "ap"
+    // cout << endl;
+    // vector<pair<string, vector<int>>> words = tree.searchPrefix("ap");
+    // printVectorOfPairs(words);
+
+    // // Vanilla implementation of "ap" prefix search
+    // // will be very slow on main file so comment this out when testing
+    // vector<string> van = vanillaSearch(lines, "ap");
+    // printVector(van);
+    // cout << endl;
+    
+    // // testing merge function
+    // PrefixTree tree2;
+    // tree2.insert("dog", 0);
+    // tree2.insert("app", 1);
+    // tree2.insert("ape", 2);
+    // tree2.insert("and", 3);
+
+    // //PrefixTree mergedTree;
+    // mergeTrees(tree, tree2);
+    // cout << "Merged Tree: " << endl;
+    // tree.print();
+
+    // MULTI THREADING:
+
+    // Array of WorkerThread objects
+    WorkerThread* worker_threads[NUM_WORKER_THREADS];
+    for (int i = 0; i < NUM_WORKER_THREADS; i++) {
+        worker_threads[i] = new WorkerThread();
     }
-    cout << "Successfully built prefix tree!" << endl;
 
-    // Print prefix tree
-    tree.print();
+    // vector of PrefixTrees from threads
+    vector<PrefixTree> threadTrees;
 
-    // Search for word "apple"
-    vector<int> indices = tree.search("apple");
-    cout << endl;
-    printVector(indices);
-
-    // Search for prefix "ap"
-    cout << endl;
-    vector<pair<string, vector<int>>> words = tree.searchPrefix("ap");
-    printVectorOfPairs(words);
-
-    // Vanilla implementation of "ap" prefix search
-    // will be very slow on main file so comment this out when testing
-    vector<string> van = vanillaSearch(lines, "ap");
-    printVector(van);
-    cout << endl;
+    int chunk_id = 0;
     
-    // testing merge function
-    PrefixTree tree2;
-    tree2.insert("dog", 0);
-    tree2.insert("app", 1);
-    tree2.insert("ape", 2);
-    tree2.insert("and", 3);
+    for (int i = 0; i < NUM_WORKER_THREADS; i++) {
+        if (worker_threads[i]->isCompleted()) {
+            if (worker_threads[i]->getID() != -1) {
 
+                // Join the thread
+                worker_threads[i]->join();
+
+                // Get the threads resulting tree
+                PrefixTree tree = worker_threads[i]->getTree();
+
+                // Add the tree to the vector
+                threadTrees.push_back(tree);
+
+                // Clear the thread for reuse
+                worker_threads[i]->clear();
+            } else {
+                // parse file in chunks
+                vector<string> lines = parseFile(args["filename"], chunk_size);
+
+                // check if end of file
+                // does end of file need to be checked for?
+                // if we already have a specified number of threads and looping through those threads only
+                // parseFile handles checking for end of file
+
+                // Delete old worker thread
+                delete worker_threads[i];
+
+                // Launch a worker thread to compress the data
+                worker_threads[i] = new WorkerThread(chunk_id);
+                worker_threads[i]->launch(lines);
+
+                chunk_id++;
+
+                //cout << "Launched thread " << i << endl;
+            }
+        }
+    }
+
+    // Join all threads remaining
+    bool all_threads_completed = false;
+    while(!all_threads_completed) {
+        all_threads_completed = true;
+        for (int i = 0; i < NUM_WORKER_THREADS; i++) {
+            if (worker_threads[i]->isCompleted()) {
+                if (worker_threads[i]->getID() != -1) {
+                    //cout << "Final join thread " << i << endl;
+
+                    // Join the thread
+                    worker_threads[i]->join();
+
+                    // Get the tree
+                    PrefixTree tree = worker_threads[i]->getTree();
+
+                    // Add tree to tree vector
+                    threadTrees.push_back(tree);
+                    
+                    // Clear the thread for reuse
+                    worker_threads[i]->clear();
+
+                    all_threads_completed = false;
+                }
+            }
+        }
+    }
+
+    // merge all trees together
     PrefixTree mergedTree;
-    mergedTree = mergeTrees(tree, tree2);
-    cout << "Merged Tree: " << endl;
+    for (int i = 0; i < threadTrees.size(); i++) {
+        mergeTrees(mergedTree, threadTrees[i]);
+    }
+
+    // final tree is in mergedTree now
+    cout << endl << "multi threading tree result: " << endl;
     mergedTree.print();
 
 }
